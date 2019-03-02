bfe84818e5f87c600c4fd0353643db1058cb698f
PashaBel <pashabel@gmail.com>
2019-03-02 09:56:22 +0300

    форма ввода и логина

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,7 +1,14 @@
 class AuthorizationController < ApplicationController
+  layout 'application'
   def index
   end
 
   def logon
+    user = User.where(user_name: params[:userName], user_password: params[:userPassword]).first
+    if user
+      render plain: "Добро пожаловать"
+    else
+      render plain: "Вы кто такие, я вас не знаю, идите ... отсюда"
+    end
   end
 end
