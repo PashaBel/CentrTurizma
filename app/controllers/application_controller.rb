aff96c6ab94559f16f4c7ddf0b19643dc60f5fd4
PashaBel <pashabel@gmail.com>
2019-03-09 12:52:58 +0300

    начало страданий с отдуплением сессий куков и всего остального дерьма

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  private
 
+  # Находим пользователя с ID, хранящимся в сессии с ключом
+  # :current_user_id Это обычный способ обрабатывать вход пользователя
+  # в приложении на Rails; вход устанавливает значение сессии, а
+  # выход убирает его.
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+        User.find_by(id: session[:current_user_id])
+  end
 end
