3dd647fc13cc23d02ce664a83d1bd8f11bc4ae2e
PashaBel <pashabel@gmail.com>
2019-03-09 13:19:54 +0300

    начало страданий с отдуплением сессий куков и всего остального дерьма

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'tour_report/new'
-  get 'tour_report/create'
-  get 'tour_report/tour_report'
   get 'authorization/index'
   get 'authorization/logon'
   root 'authorization#index'
   post 'authorization/logon'
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
