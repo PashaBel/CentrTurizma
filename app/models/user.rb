a969994dd4775d4e35796d4a8eb51b83e8c02ab1
PashaBel <pashabel@gmail.com>
2019-03-09 22:11:24 +0300

    создаем вьюшки для управления user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   belongs_to :user_type
   belongs_to :center
   def is_admin?
-    self.user_type.lookup_code == UserType::ADMIN
+    self.user_type.lookup_code == User_Type::ADMIN
   end
 end
