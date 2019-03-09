5072830686da7fa692aea6ded02d8e00b512e876
PashaBel <pashabel@gmail.com>
2019-03-09 12:56:31 +0300

    начало страданий с отдуплением сессий куков и всего остального дерьма

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -6,9 +6,20 @@ class AuthorizationController < ApplicationController
   def logon
     user = User.where(user_name: params[:userName], user_password: params[:userPassword]).first
     if user
-      render plain: "Добро пожаловать"
+      session[:current_user_id] = user.id
+      session[:UserName] = user.user_name
+      redirect_to home/index
     else
       render plain: "Вы кто такие, я вас не знаю, идите ... отсюда"
     end
+  end
+
+  # "Удаляем" логин (при выходе пользователя)
+  def destroy
+    # Убираем id пользователя из сессии
+    session.delete(:current_user_id)
+    # Очистить мемоизированного текущего пользователя
+    @_current_user = nil
+    redirect_to root_url
   end
 end
