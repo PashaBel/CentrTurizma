6b4fa0a579d2b84a4c82a0684e3f4d8985d47007
PashaBel <pashabel@gmail.com>
2019-03-11 23:13:02 +0300

    создаем вьюшки для управления user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,14 @@ class UserController < ApplicationController
 
   def index
    if current_user.is_admin?
-     DataUser = User.all
-     DataUser.each do |@user|
-
+     user = User.includes(:center)
+     @hash = []
+     user.each do |usr|
+       if usr.is_admin == true
+         @hash <<{usr_name: usr.user_name, usr_password: usr.user_password, usr_centr_name: usr.center.name, usr_type: 'Admin'}
+       else
+         @hash <<{usr_name: usr.user_name, usr_password: usr.user_password, usr_centr_name: usr.center.name, usr_type: 'User'}
+       end
      end
    else
      redirect_to :controller => 'home', :action => 'index'
