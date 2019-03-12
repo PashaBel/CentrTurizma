2552842756e12aa54d1f38b8de2803c635f4f749
PashaBel <pashabel@gmail.com>
2019-03-12 14:03:15 +0300

    Работа над issue день второй

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   post 'authorization/logon'
   get 'home/index'
   get 'user/index'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
