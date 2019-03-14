5fcc467526d5f0c1f1755acf00bb2313481c7f4e
PashaBel <pashabel@gmail.com>
2019-03-14 18:23:37 +0300

    Работа над issue день nhtnbq

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,15 +3,11 @@ class UserController < ApplicationController
   before_action :require_admin
 
   def index
-   if current_user.is_admin?
      @hash = User.all.map{ |usr| {id: usr.id, name: usr.user_name, password: usr.user_password, centr_name: usr.center&.name, type: usr.is_admin? ? 'Admin' : 'User'} }
-   else
-     redirect_to controller: :home, action: :index
-   end
   end
 
   def new
-
+    @center_list = Center.all.map{|center| [center.name, center.id]}
   end
 
   def create
