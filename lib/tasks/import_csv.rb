610e411351a67cfbee6efc3ddc05f875c2e70c1c
PashaBel <pashabel@gmail.com>
2019-03-19 18:08:12 +0300

    создаем вьюшки для управления user

--- lib/tasks/import_csv.rb
+++ lib/tasks/import_csv.rb
@@ -1,9 +1,14 @@
 require 'csv'
 
-hash = CSV.foreach('/home/pbelevich/Desktop/CITY.csv', headers: true).map(&:to_h)
-oblhash = hash.group_by { |obl| obl['OBL'] }
+filearray = CSV.foreach('/home/pbelevich/Desktop/CITY.csv', headers: true).map(&:to_h)
+oblhash = filearray.group_by { |obl| obl['OBL'] }
 oblhash.each do |oblnm, oblnm_value|
   i = oblnm
   puts i
+  rayonname = oblnm_value.group_by {|rayon| rayon['RAION']}
+  rayonname.each do |raynm, raynm_value|
+    j = raynm
+    puts j
+  end
 end
 
