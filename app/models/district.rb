1665ccd3f446097f9191de2621e1fdc5383b8e4e
PashaBel <pashabel@gmail.com>
2019-04-04 21:18:07 +0300

    Создаем начальный стек данных

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,6 +2,8 @@ class District < ApplicationRecord
   belongs_to :region
   has_many :points
 
+=begin
   validates :name, presence: { message: "Поле 'Название района' не может быть пустым!!!" }
   validates :name, uniqueness: { messege: "Такой район уже существует, выбирете другое название!!!"}
+=end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,12 @@
 
 require 'csv'
 
-i = 0
-j = 0
-n = 0
-#filename = '/Volumes/Data/Work/insert_db/CITY.csv'
-filename = '/home/pbelevich/Desktop/CITY.csv'
-filecenters = '/home/pbelevich/Desktop/Centers.csv'
+filename = '/Volumes/Data/Work/insert_db/CITY.csv'
+#filename = '/home/pbelevich/Desktop/CITY.csv'
+#filecenters = '/home/pbelevich/Desktop/Centers.csv'
 
 filearray = CSV.foreach(filename, headers: true).map(&:to_h)
-filecentersarray = CSV.foreach(filecenters, headers: true).map(&:to_h)
+#filecentersarray = CSV.foreach(filecenters, headers: true).map(&:to_h)
 
 tips = filearray.map{ |tiplist| tiplist['TIP'].gsub('.', '') }.uniq
 tips.each { |tip| LocalityType.create!(short_name: tip) }
@@ -25,17 +22,14 @@ typehash = LocalityType.pluck(:short_name, :id).to_h
 oblhash = filearray.group_by { |obl| obl['OBL'] }
 
 oblhash.each do |oblnm, oblnm_value|
-  i += 1
-  Region.create(name: oblnm)
+  regionid = Region.create(name: oblnm)
 
   rayonname = oblnm_value.group_by {|rayon| rayon['RAION']}
   rayonname.each do |raynm, raynm_value|
-    j += 1
-    District.create(region_id: i, name: raynm)
+    districtid = District.create(region_id: regionid.id, name: raynm)
 
     raynm_value.each do |nasnm|
-      n = typehash[nasnm['TIP']]
-      Locality.create(district_id: j, name: nasnm['NAME'], locality_type_id: n)
+      Locality.create(district_id: districtid.id, name: nasnm['NAME'], locality_type_id: typehash[nasnm['TIP']])
     end
   end
 end
@@ -43,6 +37,7 @@ end
 User.create(user_name: 'BelPaK', user_password: '123', center_id: 1, is_admin: 1)
 User.create(user_name: 'Alex', user_password: '321', center_id: 1, is_admin: 1)
 
+=begin
 filecentersarray.each do |elem|
   distrid = District.find_by(name: elem['rayon'])
   loc = elem['locality_centr_tur']||elem['locality_otd_obr']
@@ -50,3 +45,4 @@ filecentersarray.each do |elem|
   centrname = elem['name_centr_tur']||elem['name_otd_obr']
   Center.create(name: centrname, district_id: distrid, locality_id: locid, email: '')
 end
+=end
