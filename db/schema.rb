6b761aeecc1f94b01a4cfa61a3ffc822c852081a
PashaBel <pashabel@gmail.com>
2019-04-22 21:58:50 +0300

    Вернул тест на добавление данных и новые структуры для школ и центров

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2019_03_19_191328) do
     t.string "shortname", limit: 50, null: false
     t.string "fullname"
     t.integer "district_id", null: false
-    t.integer "locality_id", null: false
     t.string "email", limit: 50, null: false
     t.index ["shortname"], name: "index_centers_on_shortname", unique: true
   end
@@ -32,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_03_19_191328) do
     t.string "fullname"
     t.string "email", limit: 30, null: false
     t.integer "center_id", null: false
-    t.integer "locality_id", null: false
     t.index ["email"], name: "index_institutions_on_email", unique: true
     t.index ["shortname"], name: "index_institutions_on_shortname", unique: true
   end
