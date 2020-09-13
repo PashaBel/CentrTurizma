fdbff85b13e3237b5043fc0cab7b2d9a8b481e1e
PashaBel <pashabel@gmail.com>
2020-09-13 14:49:46 +0300

    Edit Seed file. Edit Center migration file. Edit Institution migrate file

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -20,9 +20,6 @@ class AuthorizationController < ApplicationController
   #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
   #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
-
   def index
     reset_session
   end
--- db/migrate/20190308165922_create_table_institutions.rb
+++ db/migrate/20190308165922_create_table_institutions.rb
@@ -1,10 +1,9 @@
 class CreateTableInstitutions < ActiveRecord::Migration[5.2]
   def self.up
     create_table :institutions do |t|
-      t.string :shortname, :limit => 50, null: false, index: { unique: true }
-      t.string :fullname, :limiit => 200
+      t.string :fullname, :limit => 200, null: false, index: { unique: true }
       t.string :email, :limit => 30, null: false, index: { unique: true }
-      t.integer :center_id, null: false
+      t.integer :district_id, null: false
     end
   end
   def self.down
